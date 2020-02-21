11311da4c7ab12f880e11020842de9265244150e
Steffen Holanger <holanger.steffen@gmail.com>
2020-02-21 12:22:05 +0100

    refactore out whale

--- src/Whale.java
+++ src/Whale.java
@@ -3,41 +3,44 @@ import java.util.Iterator;
 import java.util.Random;
 
 /**
- * A simple model of a fox.
- * Foxes age, move, eat rabbits, and die.
- * 
+ * A simple model of a whale.
+ * Whales age, move, eat krill, and die.
+ *
+ * based on:
  * @author David J. Barnes and Michael Kölling
  * @version 2016.02.29 (2)
+ *
+ * refactored by: Steffen Holanger, Sander Hurlen and Emil Elton Nilsen
  */
 public class Whale extends Animal
 {
-    // Characteristics shared by all foxes (class variables).
+    // Characteristics shared by all whales (class variables).
     
-    // The age at which a fox can start to breed.
-    private static final int BREEDING_AGE = 15;
-    // The age to which a fox can live.
-    private static final int MAX_AGE = 150;
-    // The likelihood of a fox breeding.
+    // The age at which a whale can start to breed.
+    private static final int BREEDING_AGE = 120;
+    // The age to which a whale can live.
+    private static final int MAX_AGE = 1080;
+    // The likelihood of a whale breeding.
     private static final double BREEDING_PROBABILITY = 0.08;
     // The maximum number of births.
-    private static final int MAX_LITTER_SIZE = 2;
+    private static final int MAX_LITTER_SIZE = 1;
     // The food value of a single rabbit. In effect, this is the
-    // number of steps a fox can go before it has to eat again.
-    private static final int RABBIT_FOOD_VALUE = 9;
+    // number of steps a whale can go before it has to eat again.
+    private static final int KRILL_FOOD_VALUE = 9;
     // A shared random number generator to control breeding.
     private static final Random rand = Randomizer.getRandom();
     
     // Individual characteristics (instance fields).
-    // The fox's age.
+    // The whale's age.
     private int age;
-    // The fox's food level, which is increased by eating rabbits.
+    // The whale's food level, which is increased by eating rabbits.
     private int foodLevel;
 
     /**
-     * Create a fox. A fox can be created as a new born (age zero
+     * Create a whale. A whale can be created as a new born (age zero
      * and not hungry) or with a random age and food level.
      * 
-     * @param randomAge If true, the fox will have random age and hunger level.
+     * @param randomAge If true, the whale will have random age and hunger level.
      * @param field The field currently occupied.
      * @param location The location within the field.
      */
@@ -46,27 +49,26 @@ public class Whale extends Animal
         super(field, location);
         if(randomAge) {
             age = rand.nextInt(MAX_AGE);
-            foodLevel = rand.nextInt(RABBIT_FOOD_VALUE);
+            foodLevel = rand.nextInt(KRILL_FOOD_VALUE);
         }
         else {
             age = 0;
-            foodLevel = RABBIT_FOOD_VALUE;
+            foodLevel = KRILL_FOOD_VALUE;
         }
     }
     
     /**
-     * This is what the fox does most of the time: it hunts for
-     * rabbits. In the process, it might breed, die of hunger,
+     * This is what the whale does most of the time: it hunts for
+     * krill. In the process, it might breed, die of hunger,
      * or die of old age.
-     * @param field The field currently occupied.
-     * @param newFoxes A list to return newly born foxes.
+     * @param newWhales A list to return newly born whales.
      */
-    public void act(List<Animal> newFoxes)
+    public void act(List<Animal> newWhales)
     {
         incrementAge();
         incrementHunger();
         if(isAlive()) {
-            giveBirth(newFoxes);            
+            giveBirth(newWhales);
             // Move towards a source of food if found.
             Location newLocation = findFood();
             if(newLocation == null) { 
@@ -85,7 +87,7 @@ public class Whale extends Animal
     }
 
     /**
-     * Increase the age. This could result in the fox's death.
+     * Increase the age. This could result in the whale's death.
      */
     private void incrementAge()
     {
@@ -96,7 +98,7 @@ public class Whale extends Animal
     }
     
     /**
-     * Make this fox more hungry. This could result in the fox's death.
+     * Make this whale more hungry. This could result in the whale's death.
      */
     private void incrementHunger()
     {
@@ -123,7 +125,7 @@ public class Whale extends Animal
                 Krill krill = (Krill) animal;
                 if(krill.isAlive()) {
                     krill.setDead();
-                    foodLevel = RABBIT_FOOD_VALUE;
+                    foodLevel = KRILL_FOOD_VALUE;
                     return where;
                 }
             }
@@ -132,13 +134,13 @@ public class Whale extends Animal
     }
     
     /**
-     * Check whether or not this fox is to give birth at this step.
+     * Check whether or not this whale is to give birth at this step.
      * New births will be made into free adjacent locations.
-     * @param newFoxes A list to return newly born foxes.
+     * @param newWhale A list to return newly born whales.
      */
-    private void giveBirth(List<Animal> newFoxes)
+    private void giveBirth(List<Animal> newWhale)
     {
-        // New foxes are born into adjacent locations.
+        // New whales are born into adjacent locations.
         // Get a list of adjacent free locations.
         Field field = getField();
         List<Location> free = field.getFreeAdjacentLocations(getLocation());
@@ -146,7 +148,7 @@ public class Whale extends Animal
         for(int b = 0; b < births && free.size() > 0; b++) {
             Location loc = free.remove(0);
             Whale young = new Whale(false, field, loc);
-            newFoxes.add(young);
+            newWhale.add(young);
         }
     }
         
@@ -165,7 +167,7 @@ public class Whale extends Animal
     }
 
     /**
-     * A fox can breed if it has reached the breeding age.
+     * A whales can breed if it has reached the breeding age.
      */
     private boolean canBreed()
     {
